dadcf5d6ee1982d26acd3cbba6f9814c47554f1a
Andrew Seward <awswrd@gmail.com>
2015-03-18 01:21:39 -0700

    Fixing general util Finally managed to get that function to work w/ ES-6!

--- app/utils/generalUtil.js
+++ app/utils/generalUtil.js
@@ -1,32 +1,47 @@
+var _getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+var _getRandomMember = (array) => {
+   if (array.length === 0) {
+      return null;
+    }
+
+    var index = _getRandomInt(array.length);
+    return array[index];
+};
+
+var _getRandomProperty = (obj) => {
+  var keys = Object.keys(obj);
+  if (keys.length == 0) {
+    return null;
+  }
+
+  var key = this.getRandomMember(keys);
+  return obj[key];
+};
+
+var _normalizeWhitespace = (sentence) => {
+  return sentence.trim().split(/\s/).filter((item) => {
+    return item.trim() !== '';
+  }).join(' ');
+};
+
 var generalUtil = {
   getRandomInt: (max) => {
-    return Math.floor(Math.random() * max);
+    return _getRandomInt(max);
   },
 
   getRandomMember: (array) => {
-    if (array.length === 0) {
-      return null;
-    }
-
-    var index = getRandomInt(array.length);
-    return array[index];
+    return _getRandomMember(array);
   },
 
-  // Couldn't get this to work w/ ES6 `(obj) => {...}` syntax :(
-  getRandomProperty: function(obj) {
-    var keys = Object.keys(obj);
-    if (keys.length == 0) {
-      return null;
-    }
-
-    var key = this.getRandomMember(keys);
-    return obj[key];
+  getRandomProperty: (obj) => {
+    return _getRandomProperty(obj);
   },
 
   normalizeWhitespace: (sentence) => {
-    return sentence.trim().split(/\s/).filter((item) => {
-      return item.trim() !== '';
-    }).join(' ');
+    return _normalizeWhitespace(sentence);
   }
 };
 
